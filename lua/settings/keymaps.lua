b547de7cd5b68898da28630a100e977fcc4f0e5a
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2024-06-12 17:02:34 +0300

    🚚 Chore(refact): moved vim keybindings & options to separate module

---
+++ lua/settings/keymaps.lua
@@ -0,0 +1,49 @@
+-- greatest remap ever: primeagen
+-- e.g if you don't want the copy-paste to store the removed word to the registry do:
+-- yaw -> vaw -> <leader> + p
+vim.keymap.set("x", "<leader>p", '"_dP')
+-- Neoscroll
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Remain cursor in the middle when scrolling up" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Remain cursor in the middle when scrolling down" })
+
+-- Shortcut for Explore Ex
+vim.keymap.set("n", "<C-p>", vim.cmd.Ex, { desc = "Shortcut for :Ex" })
+-- Tmux
+-- Open a pane in the current path of nvim
+vim.keymap.set("n", "<leader>`", function()
+	local current_dir = vim.fn.expand("%:p:h")
+	vim.cmd("silent !tmux split-window -v -c " .. current_dir, " -p 5")
+end, { noremap = true, silent = true })
+
+-- Undotree hotkey
+vim.keymap.set("n", "<leader><F5>", vim.cmd.UndotreeToggle)
+
+-- -- Set the path of netrw to always point to the current dir path
+vim.cmd("autocmd BufEnter * lcd %:p:h")
+
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- Diagnostic keymaps
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+-- TIP: Disable arrow keys in normal mode
+ vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+ vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+ vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+ vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--  See `:help wincmd` for a list of all window commands
+ vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+ vim.keymap.set("n", "<C-j>", "<C---[[ w><C-j>", { desc = "Move focus to the lower window" }) 
+ vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
---
+++ lua/settings/options.lua
@@ -0,0 +1,64 @@
+-- Block cursor
+vim.opt.guicursor = ""
+vim.opt.termguicolors = true
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+-- Make line numbers default
+vim.opt.number = true
+-- You can also add relative line numbers, to help with jumping.
+vim.opt.relativenumber = true
+
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.opt.mouse = "a"
+
+-- Don't show the mode, since it's already in the status line
+vim.opt.showmode = false
+
+-- Sync clipboard between OS and Neovim.
+vim.opt.clipboard = "unnamedplus"
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Color column
+vim.opt.colorcolumn = "80"
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keep signcolumn on by default
+vim.opt.signcolumn = "yes"
+
+-- Decrease update time
+vim.opt.updatetime = 250
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Set the path of netrw to always point to the current dir path
+vim.g.netrw_keepdir = 0
+
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = "split"
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
+
+-- Undo tree
+vim.opt.undofile = true
+
+-- Set highlight on search, but clear on pressing <Esc> in normal mode
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
