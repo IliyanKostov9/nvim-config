6045d4889533547bace4616f607cc6e1070152aa
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2024-06-23 20:53:06 +0300

    ⬆️ Chore: update deps

--- lua/helpers/init.lua
+++ lua/helpers/init.lua
@@ -1,9 +1,9 @@
 return {
-	require("helpers.plugins.which-key"),
-	require("helpers.plugins.todo-comments"),
-	require("helpers.plugins.better-escape"),
-	require("helpers.plugins.undotree"),
-	require("helpers.plugins.Comment"),
-	require("helpers.plugins.headiines"),
-	require("helpers.plugins.markdown-preview"),
+  require("helpers.plugins.which-key"),
+  require("helpers.plugins.todo-comments"),
+  require("helpers.plugins.better-escape"),
+  require("helpers.plugins.undotree"),
+  require("helpers.plugins.Comment"),
+  require("helpers.plugins.headiines"),
+  require("helpers.plugins.markdown-preview"),
 }
