1e788324877dd27e81ad6b192dcc22f9ad384953
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2024-07-28 12:33:34 +0300

    :bulb: Chore: removed comments

--- lua/managers/ui.lua
+++ lua/managers/ui.lua
@@ -1,19 +1,17 @@
 ui = {
-		
-		icons = vim.g.have_nerd_font and {} or {
-			cmd = "⌘",
-			config = "🛠",
-			event = "📅",
-			ft = "📂",
-			init = "⚙",
-			keys = "🗝",
-			plugin = "🔌",
-			runtime = "💻",
-			require = "🌙",
-			source = "📄",
-			start = "🚀",
-			task = "📌",
-			lazy = "💤 ",
-		},
-	}
-
+  icons = vim.g.have_nerd_font and {} or {
+    cmd = "⌘",
+    config = "🛠",
+    event = "📅",
+    ft = "📂",
+    init = "⚙",
+    keys = "🗝",
+    plugin = "🔌",
+    runtime = "💻",
+    require = "🌙",
+    source = "📄",
+    start = "🚀",
+    task = "📌",
+    lazy = "💤 ",
+  },
+}
