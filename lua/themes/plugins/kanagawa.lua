3ac4d1d9790dffe9ea953ece9aefbe2a935c6a7a
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2024-08-09 21:14:50 +0300

    :arrow_up: chore: updated deps

--- lua/themes/plugins/kanagawa.lua
+++ lua/themes/plugins/kanagawa.lua
@@ -27,25 +27,27 @@ return {
           light = "lotus",
         },
       }
+      vim.cmd.colorscheme("rose-pine-main")
       -- vim.cmd.colorscheme("kanagawa-wave")
-      local function set_color_scheme()
-        local hour = tonumber(os.date("%H"))
-        if hour >= 7 and hour < 20 then
-          vim.cmd.colorscheme("rose-pine-dawn")
-        else
-          vim.cmd.colorscheme("kanagawa-wave")
-        end
-      end
-      set_color_scheme()
-      local timer = vim.loop.new_timer()
-      timer:start(
-        0,
-        3600000,
-        vim.schedule_wrap(function()
-          set_color_scheme()
-        end)
-      )
-      vim.cmd.hi("Comment gui=none")
+      --   local function set_color_scheme()
+      --     local hour = tonumber(os.date("%H"))
+      --     if hour >= 7 and hour < 20 then
+      --       vim.cmd.colorscheme("rose-pine-dawn")
+      --     else
+      --       -- vim.cmd.colorscheme("kanagawa-wave")
+      --       vim.cmd.colorscheme("rose-pine")
+      --     end
+      --   end
+      --   set_color_scheme()
+      --   local timer = vim.loop.new_timer()
+      --   timer:start(
+      --     0,
+      --     3600000,
+      --     vim.schedule_wrap(function()
+      --       set_color_scheme()
+      --     end)
+      --   )
+      --   vim.cmd.hi("Comment gui=none")
     end,
   },
 }
