dc200b42e5556e0ef192adbfb6045b87f62cf6c9
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2025-01-19 11:14:09 +0200

    fix: woodpecker deprecation issue

--- lua/themes/init.lua
+++ lua/themes/init.lua
@@ -1,12 +1,12 @@
 return {
   require("themes.plugins.rose-pine"),
   require("themes.plugins.kanagawa"),
-  require("themes.plugins.gruvbox"),
-  require("themes.plugins.solarized"),
+  -- require("themes.plugins.gruvbox"),
+  -- require("themes.plugins.solarized"),
   require("themes.plugins.vim-devicons"),
-  require("themes.plugins.zenburn"),
-  require("themes.plugins.tomorrow-night"),
+  -- require("themes.plugins.zenburn"),
+  -- require("themes.plugins.tomorrow-night"),
   require("themes.plugins.nightfox"),
-  require("themes.plugins.material"),
-  require("themes.plugins.paper"),
+  -- require("themes.plugins.material"),
+  -- require("themes.plugins.paper"),
 }
