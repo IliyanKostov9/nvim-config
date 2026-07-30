f3e8acc40237f28e0faab623ce81e8edcd55d3cf
Kostov, Iliyan <Iliyan.Kostov@experian.com>
2026-07-30 17:56:03 +0300

    fix: markdownpreview
    M  lazy-lock.json
    M  lua/plugins/helpers/markdown-preview.lua

--- lua/plugins/helpers/markdown-preview.lua
+++ lua/plugins/helpers/markdown-preview.lua
@@ -4,18 +4,17 @@ return {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
-    build = function(plugin)
-      if vim.fn.executable("npx") then
-        vim.cmd("!cd " .. plugin.dir .. " && cd app && npx --yes yarn install")
-      else
-        vim.cmd([[Lazy load markdown-preview.nvim]])
-        vim.fn["mkdp#util#install"]()
-      end
-    end,
+    build = "cd app && npm install",
     init = function()
-      if vim.fn.executable("npx") then
-        vim.g.mkdp_filetypes = { "markdown" }
-      end
+      vim.g.mkdp_filetypes = { "markdown" }
+      vim.g.mkdp_echo_preview_url = 1
+      vim.g.mkdp_browserfunc = "OpenMarkdownPreview"
+
+      vim.cmd([[
+      function OpenMarkdownPreview(url)
+        execute "silent !open -a 'Google Chrome' " . shellescape(a:url)
+      endfunction
+      ]])
     end,
   },
 }
